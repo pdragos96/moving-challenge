cb1cc9a92818f8bb3e9d0eebbfe7c508d52fa271
Dragos Podariu <dragos.podariu@fortech.ro>
2019-07-04 11:45:40 +0300

    Changed Toolbar Font!

--- src/app/services/nav-service/nav.service.ts
+++ src/app/services/nav-service/nav.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { UserResponse } from "../../models/userResponse";
-
 import { ContestantCombined } from "../../models/contestantCombined";
 
 @Injectable({
